783ecdeae110409c3c49a052f8770162dcd3f4fa
AvAarsNS <arthur.vanaarssen@ns.nl>
2022-05-18 11:24:04 +0200

    First round test

--- src/game.js
+++ src/game.js
@@ -14,6 +14,23 @@ async function ticTacToe() {
     player = nextPlayer(player);
 
     console.log('The game will start with player ' + player);
+
+    while (!winConditionMet) {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        console.log('Player ' + player + ':');
+
+        var column, row;
+        do {
+            column = Math.floor(Math.random() * 3);
+            row = Math.floor(Math.random() * 3);
+        } while (!placeIsEmpty(board, column, row));
+
+        board = place(board, column, row, player);
+        console.log(formatBoard(board) + '\n');
+
+        winConditionMet = true;
+    }
 }
 
 ticTacToe()
