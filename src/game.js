be8b6065e78e80e01eb12392192bc1a6557c6665
AvAarsNS <arthur.vanaarssen@ns.nl>
2022-05-18 11:17:11 +0200

    First game attempt

---
+++ src/game.js
@@ -0,0 +1,7 @@
+const {createBoard, place, detectWin, boardIsFull, formatBoard, placeIsEmpty, nextPlayer} = require('./functions');
+
+async function ticTacToe() {
+    console.log('Game board creation...')
+}
+
+ticTacToe()
