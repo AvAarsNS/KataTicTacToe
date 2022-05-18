6685059002c9b6be9b1c219074523581cded76f2
AvAarsNS <arthur.vanaarssen@ns.nl>
2022-05-18 11:20:06 +0200

    Board creation phase

--- src/game.js
+++ src/game.js
@@ -1,7 +1,19 @@
 const {createBoard, place, detectWin, boardIsFull, formatBoard, placeIsEmpty, nextPlayer} = require('./functions');
 
 async function ticTacToe() {
+    var board, player;
+    var winConditionMet = false;
+
     console.log('Game board creation...')
+
+    board = createBoard();
+
+    console.log(formatBoard(board) + '\n');
+    console.log('Board Created.');
+
+    player = nextPlayer(player);
+
+    console.log('The game will start with player ' + player);
 }
 
 ticTacToe()
